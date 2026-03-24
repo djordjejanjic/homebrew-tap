f74dbdae7803672fe0a52270a0d35bb9c112f3a8
djordjejanjic <djordje.janjic@hotmail.com>
2026-03-24 19:43:19 +0100

    Update cask with SHA256 for v1.0.0

--- Casks/omni-converter.rb
+++ Casks/omni-converter.rb
@@ -1,8 +1,8 @@
 cask "omni-converter" do
   version "1.0.0"
-  sha256 :no_check  # Replace with actual SHA256 after creating release
+  sha256 "63e6688cb34ab7d5b94352c5c9ceaaaec8c28d3ddc16d391a1ba69ccce9966ab"
 
-  url "https://github.com/djordjejanjic/omni-converter/releases/download/v#{version}/omni-converter.zip"
+  url "https://github.com/djordjejanjic/omni-converter/releases/download/v#{version}/omni-converter-#{version}.zip"
   name "Omni Converter"
   desc "Lightweight macOS image converter"
   homepage "https://github.com/djordjejanjic/omni-converter"
