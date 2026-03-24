9e8fe55607d11b22cc10e8b66b2ea5d9cdc1f551
djordjejanjic <djordje.janjic@hotmail.com>
2026-03-24 19:55:34 +0100

    Update to v1.0.1 with correct zip structure

--- Casks/omni-converter.rb
+++ Casks/omni-converter.rb
@@ -1,5 +1,5 @@
 cask "omni-converter" do
-  version "1.0.0"
+  version "1.0.1"
   sha256 "20ee954113f416dc034a41e30fd80e07a8da8f60b8d89e7d7de66a7855fc30f6"
 
   url "https://github.com/djordjejanjic/omni-converter/releases/download/v#{version}/omni-converter-#{version}.zip"
