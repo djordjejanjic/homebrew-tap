1f7d3f9e131c3997bf06ed2bb5149a0e530db155
Djordje Janjic <djordje.janjic@hotmail.com>
2026-03-27 17:07:33 +0100

    Update omni-converter to v1.1

--- Casks/omni-converter.rb
+++ Casks/omni-converter.rb
@@ -1,6 +1,6 @@
 cask "omni-converter" do
-  version "1.0.1"
-  sha256 "20ee954113f416dc034a41e30fd80e07a8da8f60b8d89e7d7de66a7855fc30f6"
+  version "1.1"
+  sha256 "91d63941e0edaa92c53541c02d46768ef56ecd7af39f6d9eab90f9f6cc34aeab"
 
   url "https://github.com/djordjejanjic/omni-converter/releases/download/v#{version}/omni-converter-#{version}.zip"
   name "Omni Converter"
